4bc5879ad92becb8ec655e917bb10cc5c2bced2e
SevvalOcal <148449976+SevvalOcal@users.noreply.github.com>
2026-02-17 16:34:15 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: [ŞEVVAL ÖCAL]</p>
+      <p>Ogrenci No: [235541014]</p>
+    </div>
+  )
+}
+
+export default App
